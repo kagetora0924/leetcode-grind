9798bacf706fb5b44098fbd4f3653d70ccbb0a3e
宮本景虎 <miyamotokagetora@miyamotokagetoranoMacBook-Pro.local>
2024-05-05 11:36:51 +0900

    フロイドの循環検出法もstep2まで

--- 142.py
+++ 142.py
@@ -5,7 +5,7 @@
 #         self.next = None
 
 class Solution:
-    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def detectCycle1(self, head: Optional[ListNode]) -> Optional[ListNode]:
         visited_nodes = set()
         temp = head
         while head:
@@ -13,4 +13,25 @@ class Solution:
                 break
             visited_nodes.add(head)
             head = head.next
+        return head
+
+    def detectCycle2(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        slow = head
+        fast = head
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+
+        if not fast or not fast.next:
+            return None
+
+        while slow != head:
+            head = head.next
+            slow = slow.next
+
         return head
