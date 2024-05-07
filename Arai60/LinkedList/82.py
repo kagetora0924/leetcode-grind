4479cfebcbf312cb503e4bd16b8111d739a4011d
宮本景虎 <miyamotokagetora@miyamotokagetoranoMacBook-Pro.local>
2024-05-07 13:24:23 +0900

    新たなsolutionを3回書き直し、コメントを追加

--- Arai60/LinkedList/82.py
+++ Arai60/LinkedList/82.py
@@ -8,60 +8,29 @@
 
 
 # 18分経過し、分岐が複雑で詰まってしまいsolutionを確認した.
+
 class Solution:
+    # fakeノードをheadの前につけることで例外的処理を省く手法は、Linked Listにノードを追加したり削除したりする関数の実装でも使っていたことを思い出した。
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # # 前問との違いは、重複している数字は1つだけ残すのでなく全部削除するということ。
-        # # prev, curr, nextと3つポインタを作ってcurrとnextを比較していく.
-        # # 最初に重複があればheadを移動させるので、まずその処理を行う。
-
-        # # ０もしくは1ノードの場合そのまま返す
-        # if not head or not head.next:
-        #     return head
-        
-        # while head and head.next:
-        #     # headが重複の一部なら、重複を出るまで移動させる
-        #     if head.val != head.next.val:
-        #         # headが重複を脱しているのでbreak
-        #         break
-        #     prev_head_val = head.val
-        #     while head:
-        #         if head.val == prev_head_val:
-        #             # headが元の値と異なる値になるまで移動させる.
-        #             head = head.next
-        #         else: 
-        #             break
-        #     else:
-        #         #
-        # else:
-        #     # 重複を脱したら
-
-        # # この時点で、headが重複の一部でないことが保証されている
-        # prev_node = head
-        # curr_node = head
-        # while :
-        #     next_node = curr_node.next
-        #     while next_node and curr_node.val == next_node.val:
-        #         next_node = next_node.next
-        #     prev_node.next = next_node
-        #     prev_node = prev_node.next
-
-        # 18分経過し、分岐が複雑で詰まってしまいsolutionを確認した. fakeノードをheadの前につけることで、
-        # headについて特別な処理をしなくてよくなることが多いという手法を思い出した.
-        fake = ListNode(val=-1, next=head)
+        fake = ListNode(-1, head)
         prev = fake
         curr = head
+        # リストの最後に到達するときは必ずここの判定を通る.
         while curr:
             while curr.next and curr.val == curr.next.val:
-                # currの次があって、currと同じ値ということは重複なのでその最後までポインタを進める
+                # currが重複の一部であるなら、その端まで到達させる.
                 curr = curr.next
+
+            # この時点で、currがNoneでないと保証されている.
+
             if prev.next == curr:
-                # prevとcurrが隣ということは、重複がなかったということなのでprevもcurrも進める
+                # duplicateがなかったので、今見ているcurrは取り除く必要がないと確定し、prevも進めて良い.
                 prev = prev.next
                 curr = curr.next
             else:
-                # 重複があった場合は、重複部分を削除する.
+                # duplicateがあったので、今見ているcurrは取り除かれる必要がある.
+                # 新しいcurrについてはduplicateかわからないので、prev自体は進めない.
                 prev.next = curr.next
                 curr = prev.next
         
-        # リストの頭がどう移動していようと、fake.nextがリストの始まりになっている.
         return fake.next
